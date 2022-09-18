fce8653e7d62adadfde97503e78894bb0cc48e58
unknown <you@example.com>
2022-09-18 11:45:13 +0530

    updated data validationfile

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    logging.log("We are testing module")
     return 'Starting ml project'
 
 
